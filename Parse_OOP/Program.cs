f9cd9c50bfd539360b6a70b0af1a213ca800c862
Sonya Sergeeva <sonya451f@gmail.com>
2021-11-04 22:26:04 +0700

    Add branchS

--- Parse_OOP/Program.cs
+++ Parse_OOP/Program.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using ParseResult = System.Tuple<Parse_OOP.Expression, int>;
 
-//проверочка
+//ветка НЕ главного разработчика, но тоже не трогать пжшка (всех лавки)
 namespace Parse_OOP
 {
     // калькулятор
